d5b4026f1111d9a9c84a6fc0aa59bcd29a764ded
Atis <atis@aol.es>
2024-04-27 22:26:47 +0300

    Revert "remove framebuffer switch"
    This reverts commit ec9c20d25c39ceec7ca5b93430cf8e085f44a11f.

--- game/script/util.lua
+++ game/script/util.lua
@@ -851,6 +851,7 @@ end
 
 -- Wipe engine-side and client-side state and execute a script file.
 local function GoTo(roomName, TransitionFunc, disableESCAPE, fadeEffect)
+	eapi.SwitchFramebuffer()
 	eapi.Clear()			-- Clear state.
 
 	emptyDummy = { }
